b37efc92f4f8391da4fc913330f8695370f0b308
Mario Navarra <32093830+marionavarra@users.noreply.github.com>
2018-02-13 23:18:45 +0100

    Improvement

--- terzo.scala
+++ terzo.scala
@@ -13,7 +13,7 @@ case class LabeledText(id: Long,text: String,category: Category)
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
 case class Article(id: Long,text: String,category: Category)
-val articles = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("./maltempo.csv")
+val articles = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("./guasto.csv")
 val topic2Label: Boolean => Double = isSci => if (isSci) 1 else 0
 val toLabel = udf(topic2Label)
 val labelled = articles.withColumn("label", toLabel($"topic".like("%true%"))).cache
@@ -21,29 +21,32 @@ val Array(trainDF, testDF) = labelled.randomSplit(Array(0.75, 0.25))
 
 val tokenizer = new RegexTokenizer().setInputCol("text").setOutputCol("words")
 
-
-val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("features").setNumFeatures(500)
-
-
-val lr = new LogisticRegression().setMaxIter(1200).setRegParam(0.001)
-
-
-val pipeline = new Pipeline().setStages(Array(tokenizer, hashingTF, lr))
-val model = pipeline.fit(trainDF)
-
-val trainPredictions = model.transform(trainDF)
-val testPredictions = model.transform(testDF)
-trainPredictions.select('id, 'topic, 'text, 'label, 'prediction).show
-trainPredictions.printSchema
-
-
-
 val evaluator = new BinaryClassificationEvaluator().setMetricName("areaUnderROC")
 
+//model.clear(_)
+val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("features").setNumFeatures(600)
+val lr = new LogisticRegression().setMaxIter(10000).setRegParam(0.0028).setThreshold(0.5).setTol(1.0E-3)  
+val pipeline = new Pipeline().setStages(Array(tokenizer, hashingTF, lr))
+val model = pipeline.fit(trainDF)
+val testPredictions = model.transform(testDF)
+val areaTest = evaluator.evaluate(testPredictions, evaluatorParams)
 
 val evaluatorParams = ParamMap(evaluator.metricName -> "areaUnderROC")
 val areaTrain = evaluator.evaluate(trainPredictions, evaluatorParams)
 val areaTest = evaluator.evaluate(testPredictions, evaluatorParams)
+
+
+//96,48%
+
+
+
+
+
+
+
+
+
+
 
 
 
