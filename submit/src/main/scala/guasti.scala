bfe9dd6f2b7eef978cd7368925d8607e42bbdca8
Mario Navarra <32093830+marionavarra@users.noreply.github.com>
2018-02-16 00:57:58 +0100

    Update guasti.scala

--- submit/src/main/scala/guasti.scala
+++ submit/src/main/scala/guasti.scala
@@ -9,36 +9,42 @@ import org.apache.spark.ml.evaluation.BinaryClassificationEvaluator
 import org.apache.spark.ml.param.ParamMap
 
 object Guasti {
-def main(args: Array[String]) {
-sealed trait Category
-case object Meteo extends Category
-case object NonMeteo extends Category
-case class LabeledText(id: Long,text: String,category: Category)
-val sc = new SparkContext()
-val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+  def main(args: Array[String]) {
+    val sc = new SparkContext()
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val filename = args(0)
+    val articles = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("./guasto"+filename+".csv")
+    val topic2Label: Boolean => Double = isSci => if (isSci) 1 else 0
+    val toLabel = udf(topic2Label)
+    val labelled = articles.withColumn("label", toLabel(col("topic").like("%true%"))).cache
+    val Array(trainDF, testDF) = labelled.randomSplit(Array(0.75, 0.25))
+    
+    val tokenizer = new RegexTokenizer().setInputCol("text").setOutputCol("words")
+    
+    val evaluator = new BinaryClassificationEvaluator().setMetricName("areaUnderROC")
+    
+    //model.clear(_)
+    val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("features").setNumFeatures(600)
+    val lr = new LogisticRegression().setMaxIter(10000).setRegParam(0.0028).setThreshold(0.5).setTol(1.0E-3)  
+    val pipeline = new Pipeline().setStages(Array(tokenizer, hashingTF, lr))
+    val model = pipeline.fit(trainDF)
+    val testPredictions = model.transform(testDF)
+    
+    val evaluatorParams = ParamMap(evaluator.metricName -> "areaUnderROC")
+    val areaTest = evaluator.evaluate(testPredictions, evaluatorParams)
+    val risultati = result.select("id", "prediction", "label")
+    risultati.registerTempTable("predicted")
+    val falseNeg = sqlContext.sql("SELECT COUNT(*) as c FROM predicted WHERE label = 1.0 and prediction = 0")
+    val falsePos = sqlContext.sql("SELECT COUNT(*) as c FROM predicted WHERE label = 0.0 and prediction = 1")
+    val trueNeg = sqlContext.sql("SELECT COUNT(*) as c FROM predicted WHERE label = 0.0 and prediction = 0")
+    val truePos = sqlContext.sql("SELECT COUNT(*) as c FROM predicted WHERE label = 1.0 and prediction = 1")
+    println("False Positive = " + falsePos.head.getLong(0))
+    println("False Negative = " + falseNeg.head.getLong(0))
+    println("True Positive = " + trueNeg.head.getLong(0))
+    println("True Negative = " + trueNeg.head.getLong(0))
+    risultati.write.mode("overwrite").format("com.databricks.spark.csv").save("./"+filename+"_result.csv")
 
-case class Article(id: Long,text: String,category: Category)
-val articles = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("./guasto.csv")
-val topic2Label: Boolean => Double = isSci => if (isSci) 1 else 0
-val toLabel = udf(topic2Label)
-val labelled = articles.withColumn("label", toLabel(col("topic").like("%true%"))).cache
-val Array(trainDF, testDF) = labelled.randomSplit(Array(0.75, 0.25))
-
-val tokenizer = new RegexTokenizer().setInputCol("text").setOutputCol("words")
-
-val evaluator = new BinaryClassificationEvaluator().setMetricName("areaUnderROC")
-
-//model.clear(_)
-val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("features").setNumFeatures(600)
-val lr = new LogisticRegression().setMaxIter(10000).setRegParam(0.0028).setThreshold(0.5).setTol(1.0E-3)  
-val pipeline = new Pipeline().setStages(Array(tokenizer, hashingTF, lr))
-val model = pipeline.fit(trainDF)
-val testPredictions = model.transform(testDF)
-
-val evaluatorParams = ParamMap(evaluator.metricName -> "areaUnderROC")
-val areaTest = evaluator.evaluate(testPredictions, evaluatorParams)
-
-println("Success prediction rate: %s".format(areaTest))
-//96,48%
-}
+    println("Success prediction rate: %s".format(areaTest))
+    //96,48%
+  }
 }
