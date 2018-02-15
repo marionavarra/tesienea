8d705760141a3506a0cac8bf79c8f8c06608493c
Mario Navarra <32093830+marionavarra@users.noreply.github.com>
2018-02-15 02:29:22 +0100

    Update perceptron.scala

--- perceptron/perceptron.scala
+++ perceptron/perceptron.scala
@@ -16,7 +16,7 @@ val toLabel = udf(topic2Label)
 val labelled = dataset.withColumn("label", toLabel(col("topic").like("%true%"))).cache
 val tokenizer = new RegexTokenizer().setInputCol("text").setOutputCol("words")
 
-val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("features").setNumFeatures(200)
+val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("features").setNumFeatures(100)
 
 val word = tokenizer.transform(labelled)   
 val featurized = hashingTF.transform(word)
@@ -25,17 +25,11 @@ val data = featurized.select("label", "features")
 val splits = data.randomSplit(Array(0.8, 0.2), seed = 1234L)
 val train = splits(0)
 val test = splits(1)
-val layers = Array[Int](200, 400, 100, 50, 10, 2)
-val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(1000)
+val layers = Array[Int](100, 200, 50, 10, 2)
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(500)
 val model = trainer.fit(train)
 val result = model.transform(test)
 val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
 val predictionAndLabels = result.select("prediction", "label")
 println("Test set accuracy = " + evaluator.evaluate(predictionAndLabels))
-result.coalesce(1).
-    write.
-    format("com.databricks.spark.csv").
-    option("header", "true").
-    save("myfile.csv")
-}
-}
+dataFrame.write.format("com.databricks.spark.csv").save("./result.csv")}
