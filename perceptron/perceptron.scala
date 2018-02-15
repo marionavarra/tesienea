d0c027b33840930e6f42e7ccb7271054ab49d356
Mario Navarra <32093830+marionavarra@users.noreply.github.com>
2018-02-16 00:31:42 +0100

    Update perceptron.scala

--- perceptron/perceptron.scala
+++ perceptron/perceptron.scala
@@ -42,10 +42,10 @@ def main(args: Array[String]) {
   val falsePos = sqlContext.sql("SELECT COUNT(*) FROM predicted WHERE label = 0.0 and prediction = 1")
   val trueNeg = sqlContext.sql("SELECT COUNT(*) FROM predicted WHERE label = 0.0 and prediction = 0")
   val truePos = sqlContext.sql("SELECT COUNT(*) FROM predicted WHERE label = 1.0 and prediction = 1")
-  println("False Positive = " + falsePos)
-  println("False Negative = " + falseNeg)
-  println("True Positive = " + trueNeg)
-  println("True Negative = " + trueNeg)
+  println("False Positive = " + falsePos.value)
+  println("False Negative = " + falseNeg.value)
+  println("True Positive = " + trueNeg.value)
+  println("True Negative = " + trueNeg.value)
   risultati.write.mode("overwrite").format("com.databricks.spark.csv").save("./"+filename+"_result.csv")
   }
 }
