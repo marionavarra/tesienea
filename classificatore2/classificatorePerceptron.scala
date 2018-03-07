33add71c0eb8d3f8dcc387edfaac83055a5c5604
Mario Navarra <32093830+marionavarra@users.noreply.github.com>
2018-03-07 08:50:36 +0100

    Update classificatorePerceptron.scala

--- classificatore2/classificatorePerceptron.scala
+++ classificatore2/classificatorePerceptron.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.ml.PipelineModel 
 import org.apache.spark.sql.{SparkSession, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.ml.feature.HashingTF
+import org.apache.spark.ml.feature.RegexTokenizer
 
 object ClassifierPerceptron {
   def main(args: Array[String]) { 
@@ -9,10 +11,15 @@ object ClassifierPerceptron {
     val sameModel = PipelineModel.load("/home/dimartino/Documenti/mario/codice/tesienea/web/classificatore/public/modelli/"+filename+"_spark-perceptron-model")
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     val df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").load("/home/dimartino/Documenti/mario/codice/tesienea/web/classificatore/public/data/test_output.txt") 
-    val result = sameModel.transform(df)
-	val predizione = result.select("prediction")
+    val topic2Label: Boolean => Int = isSci => if (isSci) 1 else 0
+    val tokenizer = new RegexTokenizer().setInputCol("text").setOutputCol("words")
+    val hashingTF = new HashingTF().setInputCol(tokenizer.getOutputCol).setOutputCol("features").setNumFeatures(498)
+    val word = tokenizer.transform(df)   
+    val featurized = hashingTF.transform(word)
+    val data = featurized.select("id", "label", "features")
+    val result = sameModel.transform(data)
+    val predizione = result.select("prediction")
     println(filename + ":" + predizione.head.getDouble(0))
-
   }
 }
 
